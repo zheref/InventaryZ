a095dab9d39d3f3ec0960fb4a83d266bbc4ed688
Sergio Daniel Lozano <sergio.x.lozano.-nd@disney.com>
2017-03-31 13:05:27 -0500

    Initial Commit

---
+++ InventaryZ/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  InventaryZ
+//
+//  Created by Sergio Daniel Lozano on 3/31/17.
+//  Copyright © 2017 Sergio Daniel Lozano. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ InventaryZ/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  InventaryZ
+//
+//  Created by Sergio Daniel Lozano on 3/31/17.
+//  Copyright © 2017 Sergio Daniel Lozano. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
